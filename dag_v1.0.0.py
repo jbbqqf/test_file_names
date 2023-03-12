80b30dcb35d3875e82eedb3a77fa0da054dd90ea
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2023-03-12 16:32:35 +0100

    add a dag

---
+++ dag_v1.0.0.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+import pendulum
+
+
+my_dag = DAG(
+    "dag_v1.0.0",
+    schedule="0 * * * *",
+    start_date=pendulum.datetime(2023, 1, 1, tz="UTC"),
+    catchup=False,
+)
+
+hello = BashOperator(
+    task_id="hello",
+    dag=my_dag,
+    bash_command='echo "hello"',
+)
+
+world = BashOperator(
+    task_id="world",
+    dag=my_dag,
+    bash_command='echo "world"',
+)
+
+hello >> world
